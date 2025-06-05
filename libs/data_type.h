2e6b53e0e4612b214dc155ea918bb5080e5b6e9e
0day <1531520247@qq.com>
2025-06-05 20:01:36 +0800

    Create data_type.h

---
+++ libs/data_type.h
@@ -0,0 +1,11 @@
+// 数据类型 头文件 data_type.h
+#ifndef DATA_TYPE_H  // 头文件保护宏
+#define DATA_TYPE_H
+
+typedef unsigned long long ID;
+typedef unsigned long long SIZE;
+typedef unsigned long long INDEX;
+typedef char * STRING;
+typedef void * COMMON_MEMORY;
+
+#endif
