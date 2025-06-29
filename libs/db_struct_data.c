52b7260bc661c4443568516d94b9bf2469d02fcf
0day <1531520247@qq.com>
2025-06-29 22:31:49 +0800

    Update db_struct_data.c

--- libs/db_struct_data.c
+++ libs/db_struct_data.c
@@ -1,3 +1,31 @@
+/**
+* Created by Clion.
+* User: huc0day
+* Date: 1-1-20
+* Time: 上午11:36
+*/
+﻿
+/*
+=======================================================================================================
+NoSQL_Storage_Engine is a high-performance memory based NOSQL database engine.
+Copyright (C) 2020-2025, huc0day (Chinese name: GaoJian).
+All rights reserved.
+﻿
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+﻿
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY;   without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.    See the
+GNU General Public License for more details.
+﻿
+You should have received a copy of the GNU General Public License
+along with this program.    If not, see <https://www.gnu.org/licenses/>.
+=======================================================================================================
+*/
+
 #include "db_struct_data.h"
 
 //pthread_mutex_t global_lock_mutex; // 保护锁表的互斥锁
